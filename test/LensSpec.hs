a88baa6ccbfb71c2901cfaab3be42e7ccdc4ad0d
Markus Hauck <markus1189@gmail.com>
2013-08-11 15:51:02 +0200

    More lens exploration
    use <%~ and <<%~ to update the target, returning the new/old value along
    with the result.

--- test/LensSpec.hs
+++ test/LensSpec.hs
@@ -7,7 +7,7 @@ import Control.Monad.State (execState)
 
 import Test.Hspec
 
-data LevelWithQueue = L { _queue :: [Int] }
+data LevelWithQueue = L { _queue :: [Int], _lId :: Int }
 makeLenses ''LevelWithQueue
 
 main :: IO ()
@@ -15,7 +15,7 @@ main = hspec spec
 
 spec :: Spec
 spec = describe "Lenses" $ do
-  let level = L [1,2,3]
+  let level = L [1,2,3] 0
 
   it "can be used to access and modify the example queue" $
     (queue %~ (5:) $ level) ^. queue `shouldBe` [5,1,2,3]
@@ -26,3 +26,10 @@ spec = describe "Lenses" $ do
           queue %= (0:)
           queue %= reverse
     newLevel ^. queue `shouldBe` [4,3,2,1,0]
+
+  it "can return the new/old value alongside when setting" $ do
+    let (oldId, newLevel) = lId <<%~ (+1) $ level
+    let (newId, _) = lId <%~ (+1) $ level
+    oldId `shouldBe` 0
+    newId `shouldBe` 1
+    newLevel ^. lId `shouldBe` 1
