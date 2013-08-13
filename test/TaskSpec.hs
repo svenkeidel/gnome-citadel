2fb8edbee6f77e014d9bd82ed453de5f04abef7b
Markus Hauck <markus1189@gmail.com>
2013-08-13 22:03:00 +0200

    Update TaskSpec to use different default level
    The old level was supposed to be created via Level.fromString which is
    undefined atm

--- test/TaskSpec.hs
+++ test/TaskSpec.hs
@@ -1,26 +1,20 @@
 module TaskSpec(main, spec) where
 
 import Test.Hspec
-{-import Test.QuickCheck-}
-{-import Types-}
 import Level
 import Task
 
 import Data.Ord (comparing)
 import Control.Monad.State
 import Control.Lens ( (^.) )
+import qualified Data.Sequence as S
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
 spec = describe "A Task" $ do
-    let levelString = unlines
-            [ "###"
-            , " ##"
-            , "@ #"
-            ]
-        level = fromString levelString
+    let level = Level [] [] 1 S.empty S.empty
 
     it "can be added to a level" $ do
       let (task,level') = flip runState level $ createTask (1,1) Mine
