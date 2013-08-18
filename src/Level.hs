011c8b56f03d8e994283906b192231f5b2ddebff
Sven Keidel <svenkeidel@googlemail.com>
2013-08-18 12:17:32 +0200

    added fromString, which creates a level from a string
    The fromString function takes also a TileBuilder, which translates
    individual chars from the level string to either actors or static
    elements.
    
    I extracted the functionality of getting the next free id from
    createTask into its own function. The function freshID returns the next
    free id and increments the appropriate field in the level type.

--- src/Level.hs
+++ src/Level.hs
@@ -47,30 +47,32 @@ data Level = Level { _actors            :: M.Map Identifier Actor
                    }
 makeLenses ''Level
 
+instance Show Level where
+  show = undefined
+
 -- | smart constructor for an empty level
 emptyLevel :: Level
 emptyLevel =
-    Level
-    { _actors = M.empty
-    , _staticElements = M.empty
-    , _nextFreeId = 0
-    , _activeTaskQueue = S.empty
-    , _inactiveTaskQueue = S.empty
-    , _idToCoord = M.empty
-    , _coordToId = M.empty
-    }
+  Level
+  { _actors = M.empty
+  , _staticElements = M.empty
+  , _nextFreeId = 0
+  , _activeTaskQueue = S.empty
+  , _inactiveTaskQueue = S.empty
+  , _idToCoord = M.empty
+  , _coordToId = M.empty
+  }
 
 createTask :: Coord -> TaskType -> State Level Task
 createTask coord tType = do
   currentLevel <- get
-  let nextId = currentLevel ^. nextFreeId
-      task = Task nextId coord tType
+  nextId <- freshId
+  let task = Task nextId coord tType
       targetQueue = if isReachable coord currentLevel
                        then activeTaskQueue
                        else inactiveTaskQueue
   targetQueue %= enqueue task
   idToCoord %= M.insert nextId coord
-  nextFreeId += 1
   return task
 
 isReachable :: Coord -> Level -> Bool
@@ -114,12 +116,31 @@ numberOfActiveTasks lvl = S.length $ lvl ^. activeTaskQueue
 numberOfInactiveTasks :: Level -> Int
 numberOfInactiveTasks lvl = S.length $ lvl ^. inactiveTaskQueue
 
-fromString :: String -> Level
-fromString = undefined
+freshId :: State Level Int
+freshId = do
+  nextFreeId += 1
+  LG.use nextFreeId
+
+type TileBuilder = Char -> Either Actor StaticElement
+
+fromString :: TileBuilder -> String -> Level
+fromString builder str = execState (mapM insert coordStr) emptyLevel
+  where
+    coordStr     = concat $ (zipWith . zipWith) (,) coords (lines str)
+    coords       = [ [ (x,y) | x <- [0..] ] | y <- [0..] ] :: [[Coord]]
+    insert (coord,char)
+      | char == ' ' = return ()
+      | otherwise   = do
+          nextId <- freshId
+          case builder char of
+            Left  a -> actors         %= (M.insert nextId a)
+            Right s -> staticElements %= (M.insert nextId s)
+          idToCoord %= M.insert nextId coord
+          coordToId %= M.insertWith (++) coord [nextId]
 
 at :: Level -> Coord -> [Tile]
 at lvl coord = catMaybes $ map lookupTile ids
-    where
-        ids = M.findWithDefault [] coord (lvl ^. coordToId)
-        lookupTile ident =  toTile <$> M.lookup ident (lvl ^. actors)
-                        <|> toTile <$> M.lookup ident (lvl ^. staticElements)
+  where
+    ids = M.findWithDefault [] coord (lvl ^. coordToId)
+    lookupTile ident =  toTile <$> M.lookup ident (lvl ^. actors)
+                    <|> toTile <$> M.lookup ident (lvl ^. staticElements)
--- test/LevelSpec.hs
+++ test/LevelSpec.hs
@@ -3,21 +3,33 @@ module LevelSpec(main, spec) where
 import Test.Hspec
 import Level
 import Renderable
+import Tile
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
 spec = describe "A Level" $ do
+
+    let levelString = unlines
+          [ "###"
+          , " ##"
+          , "@ #"
+          ]
+        levelBuilder char =
+          case char of
+              '#' -> Right wall
+              '@' -> Left dwarf
+              _   -> error ("unrecognized char " ++ show char)
+        level = fromString levelBuilder levelString
+
     it "can be accessed via coordinates" $ do
-      pending
-      let levelString = unlines
-            [ "###"
-            , " ##"
-            , "@ #"
-            ]
-          level = fromString levelString
       render (head $ level `at` (1,1)) `shouldBe` '#'
       render (head $ level `at` (0,2)) `shouldBe` '@'
-      render (head $ level `at` (1,2)) `shouldBe` ' '
       render (head $ level `at` (2,0)) `shouldBe` '#'
+      null (level `at` (1,2)) `shouldBe` True
+      null (level `at` (0,1)) `shouldBe` True
+
+    it "can be rendered to a string" $ do
+      pending
+      show level `shouldBe` levelString
