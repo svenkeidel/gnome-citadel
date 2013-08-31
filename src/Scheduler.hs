e6da071c636cd229b085e37ecc51a3f38cfce6ae
Sven Keidel <svenkeidel@googlemail.com>
2013-08-31 21:14:06 +0200

    Removed the public state parameter
    This parameter was thought for the level that is passed before the
    actual value is unfolded. In the case the level is needed during the
    unfolding, a value of type (Level -> Level) can be returned.

--- src/Scheduler.hs
+++ src/Scheduler.hs
@@ -12,59 +12,58 @@ import Control.Applicative
 import Control.Monad.State hiding (state)
 
 
-data Unfold p s a
-  -- | An 'Unfold p s a' takes a public state 's', a private state 'p'
-  -- | and produces values of type 'a'.
-  = Unfold { state   :: p
-           , stepper :: s -> p -> Step a p
+data Unfold s a
+  -- | An 'Unfold s a' takes a public state 's' and produces values of type 'a'.
+  = Unfold { state   :: s
+           , stepper :: s -> Step s a
            }
 
 class Unfoldable u where
-  unfold :: s -> u s a -> Step a (u s a)
+  unfold :: u a -> Step (u a) a
 
-instance Unfoldable (Unfold p) where
-  unfold s u =
-    case (stepper u) s (state u) of
-      Yield a p -> Yield a $ u { state = p }
-      Done      -> Done
+instance Unfoldable (Unfold s) where
+  unfold u = (\s -> u { state = s }) <!> (stepper u) (state u)
 
-data Step a s
+data Step s a
   = Yield a s
   | Done
 
-instance Functor (Step a) where
-  fmap f (Yield a s) = Yield a $ f s
+instance Functor (Step s) where
+  fmap f (Yield a s) = Yield (f a) s
   fmap _ Done        = Done
 
-data Exec s a
+(<!>) :: (s -> t) -> Step s a -> Step t a
+f <!> (Yield a s) = Yield a $ f s
+_ <!> Done        = Done
+
+data Exec a
   -- | An execution wraps an unfold and hides its private state.
-  = forall u. Unfoldable u => Exec (u s a)
+  = forall u. Unfoldable u => Exec (u a)
 
 instance Unfoldable Exec where
-  unfold s (Exec u) = Exec <$> unfold s u
+  unfold (Exec u) = Exec <!> unfold u
 
-newtype Scheduler s a =
+newtype Scheduler a =
   Scheduler
-  { _execs :: [Exec s a]
+  { _execs :: [Exec a]
   }
 makeLenses ''Scheduler
 
-scheduler :: Scheduler s a
+scheduler :: Scheduler a
 scheduler = Scheduler []
 
-execution :: MonadState (Scheduler s a) m => p -> (s -> p -> Step a p) -> m ()
-execution p f = execs %= (Exec (Unfold p f) :)
+execution :: MonadState (Scheduler a) m => s -> (s -> Step s a) -> m ()
+execution s f = execs %= (Exec (Unfold s f) :)
 
-nextSteps :: (Functor m, MonadState (Scheduler s a) m)
-         => s -> m [a]
-nextSteps s = do
+nextSteps :: (Functor m, MonadState (Scheduler a) m) => m [a]
+nextSteps = do
   (steps, execs') <- go [] [] <$> use execs
   execs .= execs'
   return steps
   where
     go as es' []     = (as, es')
     go as es' (e:es) =
-      case unfold s e of
+      case unfold e of
            Yield a e' ->  go (a:as) (e':es') es
            Done       ->  go    as      es'  es
 
--- test/SchedulerSpec.hs
+++ test/SchedulerSpec.hs
@@ -12,40 +12,39 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  let e  = execution [2..10 :: Int] (\s (p:ps) -> if s `mod` p == 0 then Yield p ps else Done)
-      e' = execution "abcdefgh" (\s (p:ps) -> if ord p == s then Yield s ps else Done)
+  let unfoldList []     = Done
+      unfoldList (p:ps) = Yield p ps
+      e  = execution [1..10 :: Int] unfoldList
+      e' = execution "abcdef" (fmap ord . unfoldList)
+      list `shouldContain` subList =
+        sort list `shouldSatisfy` (sort subList `isInfixOf`)
   
   describe "A Scheduler" $ do
     it "can register executions" $ do
-      evalState (e >> nextSteps 4) scheduler `shouldBe` [2]
-      evalState (e >> nextSteps 6) scheduler `shouldBe` [2]
-      evalState (e >> e >> nextSteps 4) scheduler `shouldBe` [2,2]
+      evalState (e >> nextSteps) scheduler `shouldBe` [1]
+      evalState (e >> nextSteps >> nextSteps) scheduler `shouldBe` [2]
+      evalState (e >> e >> nextSteps) scheduler `shouldBe` [1,1]
 
     it "can handle executions with polymorphic private state" $ do
-      evalState (e >> e' >> nextSteps 97) scheduler
-        `shouldBe` [97]
+      evalState (e >> e' >> nextSteps) scheduler
+        `shouldContain` [1,97]
 
     it "can handle multiple walking paths within a level" $ do
-      let path1 = [ (x, y) | x <- [1..10 :: Int], let y = x]
-          path2 = [ (3, y) | y <- [2..10 :: Int]]
-          walk path = execution path (\s (p:ps) -> if p `elem` s then Done else Yield p ps)
-          list `shouldContain` subList =
-            sort list `shouldSatisfy` (sort subList `isInfixOf`)
+      let path1 = [ (   x, y) | x <- [1..10 :: Int], let y = x]
+          path2 = [ (10-x, y) | x <- [1..10 :: Int], let y = x]
+          walk p = execution p unfoldList
           assertions = do
             walk path1
             walk path2
-            s1 <- nextSteps [(0,0), (3, 1)]
-            lift $ s1 `shouldContain` [(1, 1), (3, 2)]
-            s2 <- nextSteps s1
-            lift $ s2 `shouldContain` [(2, 2), (3, 3)]
-            s3 <- nextSteps s2
-            lift $ s3 `shouldContain` [(3, 4)]
-            
+            s1 <- nextSteps
+            lift $ s1 `shouldContain` [(1, 1), (9, 1)]
+            s2 <- nextSteps
+            lift $ s2 `shouldContain` [(2, 2), (8, 2)]
+            s3 <- nextSteps
+            lift $ s3 `shouldContain` [(3, 3), (7, 3)]
       execStateT assertions scheduler >> return ()
 
   describe "A Execution" $ do
     it "can be exhausted" $ do
-      evalState (e >> nextSteps 4 >> nextSteps 4) scheduler
+      evalState (execution [1 :: Int] unfoldList >> nextSteps >> nextSteps) scheduler
         `shouldBe` []
-
-      
