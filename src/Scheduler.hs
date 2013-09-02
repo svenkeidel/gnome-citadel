a26596c7b787b97a920772c7bba849b7e7eaafda
Sven Keidel <svenkeidel@googlemail.com>
2013-09-02 16:54:54 +0200

    added monoid instance for execution

--- src/Scheduler.hs
+++ src/Scheduler.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE TemplateHaskell, RankNTypes, FlexibleContexts, ExistentialQuantification #-}
 module Scheduler ( execution
+                 , Exec
                  , scheduler
                  , nextSteps
                  , Step(..)
@@ -10,7 +11,7 @@ import Control.Lens.Getter(use)
 import Control.Lens.TH
 import Control.Applicative
 import Control.Monad.State hiding (state)
-
+import Data.Monoid
 
 data Unfold s a
   -- | An 'Unfold s a' takes a public state 's' and produces values of type 'a'.
@@ -42,6 +43,17 @@ data Exec a
 
 instance Unfoldable Exec where
   unfold (Exec u) = Exec <!> unfold u
+
+instance Monoid (Exec a) where
+  mempty = Exec $ Unfold () (const Done)
+  e1 `mappend` e2 =
+    Exec (Unfold (Left e1) stepper')
+    where
+      stepper' (Left e1') =
+        case unfold e1' of
+          Done        -> stepper' (Right e2)
+          Yield a e1'' -> Yield a (Left e1'')
+      stepper' (Right e2') = Right <!> unfold e2'
 
 newtype Scheduler a =
   Scheduler
