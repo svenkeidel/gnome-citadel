553893665190db32d85146c9ace5f523c3319651
Markus Hauck <markus1189@gmail.com>
2013-09-07 10:03:55 +0200

    Change ErrorT spec to use runStateT
    runStateT makes the difference between the nested
    monad stacks clearer.

--- test/ErrorTSpec.hs
+++ test/ErrorTSpec.hs
@@ -19,31 +19,36 @@ main = hspec spec
 
 spec :: Spec
 spec = describe "An Error Monad Transformer" $ do
+  let four = 4 :: Int
+      runStateT' = flip runStateT
 
-  let div :: (Integral a, MonadError ArithException m) => a -> a -> m a 
+      div :: (Integral a, MonadError ArithException m) => a -> a -> m a
       _ `div` 0 = throwError DivideByZero
       x `div` y = return $ x `P.div` y
+
       divBy :: (Integral a, MonadState a m, MonadError ArithException m) => a -> m ()
       divBy 0 = throwError DivideByZero
-      divBy y = modify (\x -> x `P.div` y)
+      divBy y = modify (`P.div` y)
 
   it "can throw errors" $ do
-    runErrorT ((4 :: Int) `div` 2) `shouldReturn` Right 2
-    runErrorT ((4 :: Int) `div` 0) `shouldReturn` Left DivideByZero
+    runErrorT (four `div` 2) `shouldReturn` Right 2
+    runErrorT (four `div` 0) `shouldReturn` Left DivideByZero
 
-  it "can be combined with the state monad" $ do
-    (runErrorT $ flip execStateT (4 :: Int) $ divBy 2) `shouldReturn` Right 2
-    (runErrorT $ flip execStateT (4 :: Int) $ divBy 0) `shouldReturn` Left DivideByZero
-    (runErrorT $ flip execStateT (4 :: Int) $ divBy 2 >> divBy 0) `shouldReturn` Left DivideByZero
-    (runErrorT $ flip execStateT (4 :: Int) $ divBy 0 >> divBy 2) `shouldReturn` Left DivideByZero
+  context "running StateT before ErrorT" $
+    it "returns the result and final state of the state monad or only an error" $ do
+       runErrorT (runStateT' four $ divBy 2) `shouldReturn` Right ((),2)
+       runErrorT (runStateT' four $ divBy 0) `shouldReturn` Left DivideByZero
+       runErrorT (runStateT' four $ divBy 2 >> divBy 0) `shouldReturn` Left DivideByZero
+       runErrorT (runStateT' four $ divBy 0 >> divBy 2) `shouldReturn` Left DivideByZero
 
-  it "has a different semantic if it is deconstructed in opposite order" $ do
-    (flip execStateT (4 :: Int) $ runErrorT $ divBy 2) `shouldReturn` 2
-    (flip execStateT (4 :: Int) $ runErrorT $ divBy 0) `shouldReturn` 4
-    (flip execStateT (4 :: Int) $ runErrorT $ divBy 2 >> divBy 0) `shouldReturn` 2
-    (flip execStateT (4 :: Int) $ runErrorT $ divBy 0 >> divBy 2) `shouldReturn` 4
+  context "running ErrorT before StateT" $
+    it "returns the result from the state action or an error, together with the last state" $ do
+      runStateT (runErrorT $ divBy 2) four `shouldReturn` (Right (), 2)
+      runStateT (runErrorT $ divBy 0) four `shouldReturn` (Left DivideByZero, 4)
+      runStateT (runErrorT $ divBy 2 >> divBy 0) four `shouldReturn` (Left DivideByZero, 2)
+      runStateT (runErrorT $ divBy 0 >> divBy 2) four `shouldReturn` (Left DivideByZero, 4)
 
-  it "can be handled if an error did occured" $ do
+  it "can be handled if an error did occur" $ do
     let ignoreDivByZero = const $ return ()
-    (runErrorT $ flip execStateT (4 :: Int) $ divBy 0 `catchError` ignoreDivByZero >> divBy 2)
-       `shouldReturn` Right 2
+    runErrorT (runStateT' four $ divBy 0 `catchError` ignoreDivByZero >> divBy 2)
+       `shouldReturn` Right ((),2)
