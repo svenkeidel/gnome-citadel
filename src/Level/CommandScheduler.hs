79da490f4cccef3b9fd385007d86e8ae1cc63896
Sven Keidel <svenkeidel@googlemail.com>
2013-09-09 16:17:40 +0200

    added documentation for the command scheduler

--- src/Level/CommandScheduler.hs
+++ src/Level/CommandScheduler.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE TemplateHaskell, RankNTypes, FlexibleContexts, ScopedTypeVariables #-}
 module Level.CommandScheduler ( CommandScheduler
+                              , LevelState
                               , level
                               , runCommandScheduler
                               , addCommand
@@ -15,6 +16,7 @@ import Level.Commands
 
 import qualified Scheduler as S
 
+-- | A level transformation that is used by the command scheduler
 type LevelState m = StateT Level m ()
 
 data CommandScheduler m =
@@ -24,14 +26,18 @@ data CommandScheduler m =
   }
 makeLenses ''CommandScheduler
 
+-- | Performs a sequence of transformations based on commands on a level.
 runCommandScheduler :: (Monad m) => StateT (CommandScheduler m) m a -> Level -> m Level
 runCommandScheduler s lvl = do
   cmdSched <- execStateT s (CommandScheduler S.empty lvl)
   return $ cmdSched ^. level
 
+-- | Adds a command to a schedule. The command is not executed immediately.
 addCommand :: (Monad m, MonadState (CommandScheduler m) ms) => Command (LevelState m) -> ms ()
 addCommand c = scheduler %= (execState $ S.add c)
 
+-- | Applies the upcomming sequence of commands to the level. Lifts
+-- all effects into the base monad of the state transformer.
 executeGameStep :: (Functor m, Monad m) => StateT (CommandScheduler m) m ()
 executeGameStep = do
   transformations <- zoom scheduler S.next
--- src/Scheduler.hs
+++ src/Scheduler.hs
@@ -19,12 +19,18 @@ newtype Scheduler u a =
   }
 makeLenses ''Scheduler
 
+-- | creates an empty scheduler
 empty :: Scheduler u a
 empty = Scheduler []
 
+-- | adds an unfold to the scheduler.
 add :: MonadState (Scheduler u a) m => u a -> m ()
 add u = unfolds %= (u:)
 
+-- | unfolds the all stored 'Unfold's and returns all produced values
+-- in a list. The order in which the produced values of the different
+-- unfolds are returned may change between different calls of this
+-- function.
 next :: (U.Unfoldable u, Functor m, MonadState (Scheduler u a) m) => m [a]
 next = do
   (steps, unfolds') <- go [] [] <$> use unfolds
