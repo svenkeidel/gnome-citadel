8977b157664fa28a8bf65bd50f748ea53d5bbc88
Sven Keidel <svenkeidel@googlemail.com>
2013-09-12 16:56:28 +0200

    Added Unfold Transformer

--- test/UnfoldSpec.hs
+++ test/UnfoldSpec.hs
@@ -1,11 +1,13 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ScopedTypeVariables, RankNTypes, FlexibleInstances #-}
 module UnfoldSpec(main, spec) where
 
 import Test.Hspec
 import Test.QuickCheck
 import Control.Applicative
 import Data.Monoid
+import Data.Functor.Identity
+import Data.Function(on)
 
 import Unfold
 
@@ -15,6 +17,16 @@ main = hspec spec
 instance Arbitrary a => Arbitrary (Unfold a) where
   arbitrary = unfold <$> (vector 10 :: Gen [a]) <*> pure unfoldList
   shrink    = fmap (\l -> unfold l unfoldList) . shrink . toList
+
+instance (Arbitrary a) => Arbitrary (UnfoldT Identity a) where
+  arbitrary = (UnfoldT . return) <$> arbitrary
+  shrink    = fmap (UnfoldT . Identity) . shrink . runIdentity . runUnfoldT
+
+instance Show a => Show (UnfoldT Identity a) where
+  show = show . runIdentity . runUnfoldT
+
+instance Eq a => Eq (UnfoldT Identity a) where
+  (==) = (==) `on` (runIdentity . runUnfoldT)
 
 spec :: Spec
 spec = do
@@ -26,82 +38,112 @@ spec = do
       let Yield a2 _ = next u1
       a2 `shouldBe` 2
 
-    context "when used as a Functor" $ do
+    functorProperty fromList toList
+    applicativeProperty fromList toList
+    monadProperty fromList toList
+    monoidProperty fromList toList
 
-      it "mappes the values correctly" $
-        property $ \(l :: [Int]) -> toList (fmap (*2) (unfold l unfoldList)) == [2*x | x <- l]
+  describe "An UnfoldT" $ do
 
-      it "preserves identity" $
-        property $ \(u :: Unfold Int) -> (fmap id u) == u
+    let fromListT = UnfoldT . Identity . fromList
+        toListT   = toList . runIdentity . runUnfoldT
 
-      it "is a homomorphism" $
-        property $ \(i :: Int) (j :: Int) (u :: Unfold Int) ->
-          let f = (*i)
-              g = (+j)
-          in fmap (f . g) u == fmap f (fmap g u)
+    functorProperty fromListT toListT
+    applicativeProperty fromListT toListT
+    monadProperty fromListT toListT
+    monoidProperty fromListT toListT
 
-    context "when used as an Applicative" $ do
 
-      it "mappes the values correctly" $
-        property $ \(l :: [Int]) ->
-          let u = unfold l unfoldList
-          in toList ((*) <$> pure 2 <*> u) == [2*x | x <- l]
+functorProperty :: forall f. (Show (f Int), Eq (f Int), Functor f, Arbitrary (f Int))
+                => (forall a. [a] -> f a) -> (forall a. f a -> [a]) -> Spec
+functorProperty fromList' toList' =
+  context "when used as a Functor" $ do
 
-      it "preserves identity" $
-        property $ \(u :: Unfold Int) -> toList (pure id <*> u) == toList u
+    it "mappes the values correctly" $
+      property $ \(l :: [Int]) -> toList' (fmap (*2) (fromList' l)) == [2*x | x <- l]
 
-      it "is closed under composition" $
-        property $ \(i :: Int) (j :: Int) (u :: Unfold Int) ->
-          let f = pure (*i)
-              g = pure (+j)
-          in (pure (.) <*> f <*> g <*> u) == (f <*> (g <*> u))
+    it "preserves identity" $
+      property $ \(u :: f Int) -> (fmap id u) == u
+
+    it "is a homomorphism" $
+      property $ \(i :: Int) (j :: Int) (u :: f Int) ->
+      let f = (*i)
+          g = (+j)
+      in fmap (f . g) u == fmap f (fmap g u)
+
+
+applicativeProperty :: forall f. (Show (f Int), Eq (f Int), Applicative f, Arbitrary (f Int))
+                    => (forall a. [a] -> f a) -> (forall a. f a -> [a]) -> Spec
+applicativeProperty fromList' toList' =
+  context "when used as an Applicative" $ do
+
+    it "mappes the values correctly" $
+      property $ \(l :: [Int]) ->
+      let u = fromList' l
+      in toList' ((*) <$> pure 2 <*> u) == [2*x | x <- l]
+
+    it "preserves identity" $
+      property $ \(u :: f Int) -> toList' (pure id <*> u) == toList' u
+
+    it "is closed under composition" $
+      property $ \(i :: Int) (j :: Int) (u :: f Int) ->
+      let f = pure (*i)
+          g = pure (+j)
+      in (pure (.) <*> f <*> g <*> u) == (f <*> (g <*> u))
       
-      it "is an homomorphism" $
-        property $ \(i :: Int) (x :: Int) ->
-          let f = (*i)
-          in (pure f <*> pure x :: Unfold Int) == (pure (f x))
+    it "is an homomorphism" $
+      property $ \(i :: Int) (x :: Int) ->
+      let f = (*i)
+      in (pure f <*> pure x :: f Int) == (pure (f x))
 
-      it "can interchange function and argument" $
-        property $ \(i :: Int) (x :: Int) ->
-          let f = pure (*i)
-          in (f <*> pure x :: Unfold Int) == (pure ($ x) <*> f)
+    it "can interchange function and argument" $
+      property $ \(i :: Int) (x :: Int) ->
+      let f = pure (*i)
+      in (f <*> pure x :: f Int) == (pure ($ x) <*> f)
 
-    context "when used as a monad" $ do
+monadProperty :: forall f. (Show (f Int), Eq (f Int), Monad f, Arbitrary (f Int))
+                    => (forall a. [a] -> f a) -> (forall a. f a -> [a]) -> Spec
+monadProperty fromList' toList' =
+  context "when used as a monad" $ do
 
-      it "binds correctly" $
-        property $ \(l :: [Int]) ->
-          let u = unfold l unfoldList
-          in toList (u >>= (\a -> return $ 2 * a)) == [2*x | x <- l]
+    it "binds correctly" $
+      property $ \(l :: [Int]) ->
+        let u = fromList' l
+        in toList' (u >>= (\a -> return $ 2 * a)) == [2*x | x <- l]
 
-      it "preserves left application" $
-        property $ \(i :: Int) (x :: Int) ->
-          let f = return . (*i)
-          in (return x >>= f :: Unfold Int) == f x
+    it "preserves left application" $
+      property $ \(i :: Int) (x :: Int) ->
+        let f = return . (*i)
+        in (return x >>= f :: f Int) == f x
 
-      it "preserves right identity" $
-        property $ \(u :: Unfold Int) ->
-          (u >>= return) == u
+    it "preserves right identity" $
+      property $ \(u :: f Int) ->
+        (u >>= return) == u
       
-      it "preserves associativity" $
-        property $ \(i :: Int) (j :: Int) (u :: Unfold Int) ->
-          let f = return . (*i)
-              g = return . (+j)
-          in (u >>= (\x -> f x >>= g)) == ((u >>= f) >>= g)
+    it "preserves associativity" $
+      property $ \(i :: Int) (j :: Int) (u :: f Int) ->
+        let f = return . (*i)
+            g = return . (+j)
+        in (u >>= (\x -> f x >>= g)) == ((u >>= f) >>= g)
 
-    context "when used as a Monoid" $ do
-      it "appends the values correctly" $
-        property $ \(l :: [Int]) (k :: [Int]) ->
-          let list l' = unfold l' unfoldList
-          in toList (list l `mappend` list k) == l `mappend` k
+
+monoidProperty :: forall f. (Show (f Int), Eq (f Int), Monoid (f Int), Arbitrary (f Int))
+                    => (forall a. [a] -> f a) -> (forall a. f a -> [a]) -> Spec
+monoidProperty fromList' toList' =
+  context "when used as a Monoid" $ do
+    it "appends the values correctly" $
+      property $ \(l :: [Int]) (k :: [Int]) ->
+        let list l' = fromList' l'
+        in toList' (list l `mappend` list k) == l `mappend` k
       
-      it "preserves right identity" $
-        property $ \(u :: Unfold Int) ->
-          u `mappend` mempty == u
+    it "preserves right identity" $
+      property $ \(u :: f Int) ->
+        u `mappend` mempty == u
 
-      it "preserves left identity" $
-        property $ \(u :: Unfold Int) ->
-          mempty `mappend` u == u
+    it "preserves left identity" $
+      property $ \(u :: f Int) ->
+        mempty `mappend` u == u
 
-      it "preserves associativity" $
-        property $ \(u :: Unfold Int) (v :: Unfold Int) (w :: Unfold Int)->
-          (u `mappend` v) `mappend` w == u `mappend` (v `mappend` w)
+    it "preserves associativity" $
+      property $ \(u :: f Int) (v :: f Int) (w :: f Int)->
+        (u `mappend` v) `mappend` w == u `mappend` (v `mappend` w)
