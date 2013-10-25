4f46c77b89c56fb338d1b29138f01859d6df3fb7
Sven Keidel <svenkeidel@googlemail.com>
2013-10-25 13:36:05 +0200

    implemented some taskmanager funcs

--- src/TaskManagement.hs
+++ src/TaskManagement.hs
@@ -6,23 +6,35 @@ module TaskManagement ( TaskManager
                       , active
                       , inactive
                       , taskAssignment
+                      , addTask
+                      , canBeDoneBy
+                      , bestForTheJob
+                      , assignTasks
+                      , assignTo
                       ) where
 
+import Control.Lens(zoom,contains)
 import Control.Lens.TH
 import Control.Lens.Operators
+import Control.Monad.State
+import Control.Applicative
 
 import Data.Default
+import Data.Maybe(listToMaybe)
+import Data.List(sortBy)
+import Data.Ord(comparing)
 import qualified Data.Set as Set
 import qualified Data.Map as M
+import qualified Data.Foldable as F
 
 import Actor(Actor)
+import qualified Actor
 import Counter
 import Task
--- import Types
--- import Utils
-import Level.Scheduler hiding (executeGameStep)
-import qualified Level.Scheduler as CS
-import Level hiding (isReachable, _nextFreeId)
+import Level hiding (actors, isReachable, _nextFreeId)
+import qualified Level
+import Level.Scheduler(CommandScheduler)
+import qualified Level.Scheduler as Scheduler
 
 -- | Lifecycle of a task
 --
@@ -50,127 +62,60 @@ data TaskManager = TaskManager { _inactive :: Set.Set Task
                                , _reachableBy :: Level -> Task -> Actor -> Bool
                                , _taskAssignment :: M.Map (Identifier Actor) (Identifier Task)
                                , _nextFreeId :: Counter
-                               , _cmdScheduler :: CommandScheduler
                                }
 makeLenses ''TaskManager
 
-instance Show TaskManager where
-  show tm = "TaskManager { "
-         ++ "commandScheduler = " ++ show (tm ^. cmdScheduler)
-         ++ " }"
-
 taskManager :: TaskManager
 taskManager = TaskManager { _inactive       = Set.empty
-                              , _active         = Set.empty
-                              , _taskAssignment = M.empty
-                              , _reachableBy    = error "TaskManager.reachableBy undefined"
-                              , _nextFreeId     = def
-                              , _cmdScheduler   = CS.empty
-                              }
-{-
-runTaskManager :: (Monad m) => StateT TaskManager m a -> Level -> m Level
-runTaskManager s lvl = do
-  tskMgr <- execStateT s $ taskManager lvl
-  return $ tskMgr ^. level
+                          , _active         = Set.empty
+                          , _taskAssignment = M.empty
+                          , _reachableBy    = error "TaskManager.reachableBy undefined"
+                          , _nextFreeId     = def
+                          }
 
-
-level :: Lens' TaskManager Level
-level = cmdScheduler . CS.level
-
-
-addTask :: (Functor m, Monad m) => (Identifier Task -> Task) -> StateT TaskManager m ()
-addTask t = do
+-- | Adds a task to the task manager. The task is initial inactive.
+addTask :: (Identifier Task -> Task) -> TaskManager -> TaskManager
+addTask t tm = flip execState tm $ do
   task <- t <$> zoom nextFreeId freshId
   inactive %= Set.insert task
 
-numberOfTasks :: LG.Getter TaskManager Int
-numberOfTasks = LG.to $ \tm ->
-  tm ^. numberOfActiveTasks + tm ^. numberOfInactiveTasks
-
-numberOfActiveTasks :: LG.Getter TaskManager Int
-numberOfActiveTasks = LG.to $ \tm ->
-  DS.length $ tm ^. activeTaskQueue
-
-numberOfInactiveTasks :: LG.Getter TaskManager Int
-numberOfInactiveTasks = LG.to $ \tm ->
-  DS.length $ tm ^. inactiveTaskQueue
-
-hasTask :: Identifier Task -> LG.Getter TaskManager Bool
-hasTask tId = LG.to $ \tm ->
-  DF.any match (tm ^. activeTaskQueue) ||
-  DF.any match (tm ^. inactiveTaskQueue)
+-- | Determines if the given task can be done by the given actor
+canBeDoneBy :: Task -> Actor -> Level -> TaskManager -> Bool
+canBeDoneBy task actor lvl tm = hasAbility && not busy && reachable
   where
-    match t = t ^. Task.id == tId
+    busy       = M.notMember (actor ^. Actor.id) (tm ^. taskAssignment)
+    hasAbility = actor ^. Actor.abilities . contains (task ^. taskType)
+    reachable  = _reachableBy tm lvl task actor
 
-
-getTask :: Identifier Task -> LG.Getter TaskManager (Maybe Task)
-getTask tId = LG.to foundTask
+-- | Searches from a list of actors the actor that is best suited for
+-- the task. This implies that the actor has the proper abilities and
+-- no other actor is physically nearer located to the task (this does
+-- not mean that he has the shortest path to the task).
+bestForTheJob :: Task -> [Actor] -> Level -> TaskManager -> Maybe Actor
+bestForTheJob task actors lvl tm =
+  listToMaybe $ sortBy distanceToTask
+              $ filter (\actor -> canBeDoneBy task actor lvl tm) actors
   where
-    foundTask :: TaskManager -> Maybe Task
-    foundTask tm = useFirst [ tm ^. findTaskInQueue activeTaskQueue
-                            , tm ^. findTaskInQueue inactiveTaskQueue
-                            ]
+    distanceToTask = comparing $ \actor ->
+      distance (lvl ^. coordOf actor) (task ^. Task.target)
 
-    findTaskInQueue :: LG.Getter TaskManager (Queue Task) -> LG.Getter TaskManager (Maybe Task)
-    findTaskInQueue queue = LG.to $ \tm -> findTask tm queue (matchId tId)
-
-matchId :: Identifier Task -> Task -> Bool
-matchId tId task = tId == task ^. Task.id
-
-findTask :: TaskManager -> LG.Getter TaskManager (Queue Task) -> (Task -> Bool) -> Maybe Task
-findTask manager queue p = findOf (queue . each) p manager
-
-assignTasks :: StateT TaskManager m ()
-assignTasks = undefined
-
-
-inactiveToActive :: TaskManager -> TaskManager
-inactiveToActive tm =
-  foldlOf (inactive.each) go tm
+-- | Looks through the list of inactive tasks and tries to
+-- automatically assign those to idle dwarves.
+assignTasks :: Level -> (CommandScheduler, TaskManager) -> (CommandScheduler, TaskManager)
+assignTasks lvl (cmdScheduler0, tm0) = F.foldl go (cmdScheduler0, tm0) (tm0 ^. inactive)
   where
-    go tm task =
-      maybe tm
-            (execState . assignTo task)
-            (bestForTheJob task (tm ^. level . actors . each) tm)
-      where
-        assignTo task actor = do
-          taskAssignment    %= M.insert (actor^.id) (task^.id)
-          zoom cmdScheduler (addCommand $ task ^. Task.command)
+    go :: (CommandScheduler, TaskManager) -> Task -> (CommandScheduler, TaskManager)
+    go (cmdScheduler, tm) task =
+      case bestForTheJob task (M.elems $ lvl ^. Level.actors) lvl tm of
+        Just actor -> assignTo task actor (cmdScheduler, tm)
+        Nothing    -> (cmdScheduler, tm)
 
-executeGameStep :: (Functor m, Monad m) => StateT TaskManager (ErrorT LevelError m) ()
-executeGameStep = zoom cmdScheduler CS.executeGameStep
-
-isReachableBy :: Task -> Actor -> TaskManager -> Bool
-isReachableBy task actor tm =
-  let lvl = tm ^. level
-  in _reachableBy tm lvl task actor
-
-busy :: Actor -> LG.Getter TaskManager Bool
-busy actor = LG.to $ \tm ->
-  M.notMember (actor ^. Actor.id) (tm ^. taskAssignment)
-
-
-bestForTheJob :: Task -> [Actor] -> TaskManager -> Maybe Actor
-bestForTheJob task actors tm =
-  listToMaybe $ sortByDistanceToTarget
-              $ filter (\actor -> canBeDoneBy task actor tm) actors
+-- | Assign the given task to the given actor and add the appropriate
+-- command to the command scheduler.
+assignTo :: Task -> Actor -> (CommandScheduler, TaskManager) -> (CommandScheduler, TaskManager)
+assignTo task actor (cmdScheduler, tm) = (cmdScheduler', tm')
   where
-    sortByDistanceToTarget :: [Actor] -> [Actor]
-    sortByDistanceToTarget actors =
-      sortBy (distanceToTask $ tm ^. level) actors
-      where
-        distanceToTask lvl = comparing $ \actor ->
-          distance (lvl ^. coordOf actor) (task ^. Task.target)
-
-    canBeDoneBy :: Task -> Actor -> Bool
-    canBeDoneBy task actor =
-
-      hasAbility actor
-      &&
-      notBusy actor
-      &&
-      isReachableBy task actor tm
-
-    notBusy actor = not $ tm ^. busy actor
-    hasAbility actor = actor ^. Actor.abilities . contains (task ^. taskType)
--}
+    tm' = tm & inactive       %~ Set.delete task
+             & active         %~ Set.insert task
+             & taskAssignment %~ M.insert (actor ^. Actor.id) (task ^. Task.id)
+    cmdScheduler' = Scheduler.addCommand (Task._command task actor) cmdScheduler
--- test/LevelTransSpec.hs
+++ test/LevelTransSpec.hs
@@ -6,7 +6,6 @@ import Control.Monad.Error
 
 import Test.Hspec
 
-import Utils (fromRight)
 import Level
 import Level.Transformation
 import TestHelper
@@ -22,67 +21,55 @@ spec = describe "A Level Transformation" $ do
                       , "@  "
                       ]
       dwarf' = findDwarf level
+      wall'  = findWall (1, 0) level
+      move' coord = ErrorT . return . move dwarf' (from2d coord)
+      mine' coord = ErrorT . return . mine dwarf' (findWall coord level)
+      failOnMissingItem' coord = ErrorT . return . failOnMissingItem dwarf' wall' (from2d coord)
 
   describe "A Move" $ do
 
     it "moves an actor in one step to an adjacent coordinate" $ do
-      let mover = fromRight . move dwarf' (from2d (1,2))
-      show (mover level) `shouldBe` unlines [ "## "
-                                            , " # "
-                                            , " @ "
-                                            ]
-      let mover' = fromRight . move dwarf' (from2d (0,1))
-      show (mover' level) `shouldBe` unlines [ "## "
-                                             , "@# "
-                                             , "   "
-                                             ]
+      e <- runErrorT
+            $  move' (1,2)
+           >=> levelShouldBe [ "## "
+                             , " # "
+                             , " @ "
+                             ]
+           >=> move' (0,1)
+           >=> levelShouldBe [ "## "
+                             , "@# "
+                             , "   "
+                             ]
+            $ level
+      e `shouldSatisfy` isRight
 
     it "cannot be executed if the destination is blocked" $ do
-      let result = move dwarf' (from2d (1,1)) level
-      result `shouldSatisfy` isLeft
+      e <- runErrorT $ move' (1,1) level
+      e `shouldSatisfy` isLeft
 
-  describe "Mining" $ do
+  describe "Mining" $
     it "removes the mining target and places the actor on the empty field" $ do
-      let wall = findWall (1,1) level
-          wall' = findWall (0,0) level
-          mine' w = ErrorT . return . mine dwarf' w
-      e <- runErrorT $
-        -- ErrorT :: m (Either a b) -> ErrorT a m b
-        mine' wall
-        >=>
-        levelShouldBe [ "## "
-                       , " @ "
-                       , "   "
-                       ]
-        >=>
-        mine' wall'
-        >=>
-        levelShouldBe [ "@# "
-                       , "   "
-                       , "   "
-                       ]
-         $ level
+      e <- runErrorT
+            $  mine' (1,1)
+           >=> levelShouldBe [ "## "
+                             , " @ "
+                             , "   "
+                             ]
 
+           >=> mine' (0,0)
+           >=> levelShouldBe [ "@# "
+                             , "   "
+                             , "   "
+                             ]
+            $ level
       e `shouldSatisfy` isRight
 
   describe "Fail on item missing" $ do
-    let wall = findWall (1,1) level
-
     it "fails if the item is not on the specified location" $ do
-      let e = failOnMissingItem dwarf' wall (from2d (2,2)) level
+      let e = failOnMissingItem dwarf' wall' (from2d (2,2)) level
       e `shouldSatisfy` isLeft
 
     it "works with alternative" $ do
-      let move' = ErrorT . return . move dwarf' (from2d (1,2))
-      e <- runErrorT $
-        ErrorT . return . failOnMissingItem dwarf' wall (from2d (1,1))
-        >=>
-        move'
-        >=>
-        levelShouldBe [ "## "
-                       , " # "
-                       , " @ "
-                       ]
-         $ level
-
+      e <- runErrorT $ failOnMissingItem' (1, 0) level
       e `shouldSatisfy` isRight
+
