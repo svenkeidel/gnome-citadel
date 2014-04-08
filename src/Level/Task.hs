abba5a5cca7263089ed5f200f2d64c9fe4c5dff1
Sven Keidel <svenkeidel@googlemail.com>
2014-04-08 22:15:22 +0200

    implemented error handling for mining task

--- src/Level/Task.hs
+++ src/Level/Task.hs
@@ -3,17 +3,24 @@ module Level.Task where
 import Control.Lens.Operators
 
 import Level
+import qualified Level.Command as Command
 import StaticElement(StaticElement)
 import Task
 import Counter
 import Actor (TaskType(Mine))
-import Control.Exception(assert)
+import Unfold
+import Level.Transformation
 
 mine :: StaticElement -> Level -> Identifier Task -> Task
 mine s lvl i = Task
              { _id = i
              , _target = lvl ^. coordOf s
              , _taskType = Mine
-             , _command = assert False undefined --Command.mine s
+             , _command = \dwarf lvl' -> Command.mine s dwarf lvl' `catch` handler
              , _precondition = \lvl' -> isReachable (lvl' ^. coordOf s) lvl'
              }
+  where
+    catch :: Unfold LevelTrans -> (LevelError -> TaskStatus) -> Unfold (Level -> TaskStatus)
+    catch cmd h = fmap (\cmd' lvl' -> either h InProgress (cmd' lvl')) cmd
+
+    handler _ = Reschedule
