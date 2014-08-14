1dcac2e6d6642ca93cceef5368ada80ff3259161
Markus Hauck <markus1189@gmail.com>
2014-08-14 10:25:25 +0200

    Move functions out of where clause in gui

--- ui/Main.hs
+++ ui/Main.hs
@@ -5,14 +5,15 @@ import           Control.Lens (_1, _2, preview, _head, folded, to)
 import           Control.Lens.Operators
 import           Control.Lens.TH
 import           Graphics.Vty
+import           Data.Default (Default(def))
 
 import           Counter (Counter, Identifier)
 import qualified Counter as C
-import           Data.Default (Default(def))
 import           Level
 import qualified Level.Task as LevelTask
 import           Renderable
 import           StaticElement (StaticElement)
+import           Task (Task)
 import           TaskManagement
 import           TestHelper (createLevel)
 import           Tile (TileRepr(toTile))
@@ -25,6 +26,9 @@ data GameState = GameState { _cursor :: (Int,Int)
                            }
 makeLenses ''GameState
 
+instance Default GameState where
+  def = GameState def emptyLevel empty ["Game started."] def
+
 main :: IO ()
 main = do
   vty <- mkVty
@@ -34,45 +38,47 @@ main = do
                                       ,"  m  m  "
                                       ]
 
-  eventLoop vty (GameState (0,0) lvlInit empty ["Game started."] def)
+  eventLoop vty (def & level .~ lvlInit)
   shutdown vty
 
-  where
-    addTask' task s = let (tid,s') = freshId s
-                          taskManagerWithTask = addTask (task tid) (s ^. taskManager)
-                          ts' = assignTasks (s ^. level) taskManagerWithTask
-                      in s' & taskManager .~ ts'
+onKeyPressed :: Monad m => t -> Char -> GameState -> m GameState
+onKeyPressed _ c state@(GameState csr lvl tm _ _) =
+  case c of
+       '.' -> return $ logMessages state $ executeGameStep lvl tm
+       'm' -> return $ case findWall csr lvl of
+         Just w  -> addTask' (LevelTask.mine w lvl) state
+         Nothing -> errorMessage "Mining target not mineable" state
+       'h' -> return $ moveCursor state (-1) 0
+       'j' -> return $ moveCursor state  0   1
+       'k' -> return $ moveCursor state  0 (-1)
+       'l' -> return $ moveCursor state  1   0
+       _   -> return state
 
+moveCursor :: GameState -> Int -> Int -> GameState
+moveCursor (GameState (x,y) lvl t msgs c) dx dy = GameState (x',y') lvl t msgs c
+   where
+       x' = min (max 0 (x + dx)) (lvl ^. bounds . _1)
+       y' = min (max 0 (y + dy)) (lvl ^. bounds . _2)
 
-    onKeyPressed _ c state@(GameState csr lvl tm _ _) =
-      case c of
-           '.' -> return $ logMessages state $ executeGameStep lvl tm
-           'm' -> return $ case findWall csr lvl of
-             Just w  -> addTask' (LevelTask.mine w lvl) state
-             Nothing -> errorMessage "Mining target not mineable" state
-           'h' -> return $ moveCursor state (-1) 0
-           'j' -> return $ moveCursor state  0   1
-           'k' -> return $ moveCursor state  0 (-1)
-           'l' -> return $ moveCursor state  1   0
-           _   -> return state
-
-    moveCursor (GameState (x,y) lvl t msgs c) dx dy = GameState (x',y') lvl t msgs c
-       where
-           x' = min (max 0 (x + dx)) (lvl ^. bounds . _1)
-           y' = min (max 0 (y + dy)) (lvl ^. bounds . _2)
-
-    eventLoop vty state = do
-      update vty (drawGame state)
-      e <- next_event vty
-      case e of
-        EvKey k _ ->
-          case k of
-            (KASCII 'q') -> return ()
-            (KASCII c)   -> do
-              state' <- onKeyPressed vty c state
-              eventLoop vty state'
-            _ -> eventLoop vty state
+eventLoop :: Vty -> GameState -> IO ()
+eventLoop vty state = do
+  update vty (drawGame state)
+  e <- next_event vty
+  case e of
+    EvKey k _ ->
+      case k of
+        (KASCII 'q') -> return ()
+        (KASCII c)   -> do
+          state' <- onKeyPressed vty c state
+          eventLoop vty state'
         _ -> eventLoop vty state
+    _ -> eventLoop vty state
+
+addTask' :: (Identifier a -> Task) -> GameState -> GameState
+addTask' task s = let (tid,s') = freshId s
+                      taskManagerWithTask = addTask (task tid) (s ^. taskManager)
+                      ts' = assignTasks (s ^. level) taskManagerWithTask
+                  in s' & taskManager .~ ts'
 
 errorMessage :: String -> GameState -> GameState
 errorMessage str = messages %~ (str :)
@@ -90,14 +96,13 @@ abortedTaskMessages :: [AbortedTask] -> [String]
 abortedTaskMessages = map (\ (AbortedTask _ msg) -> msg)
 
 findWall :: (Int, Int) -> Level -> Maybe StaticElement
-findWall c lvl = preview _head
-               $ findStaticElement (\t -> render (toTile t) == '#'
-                                && lvl ^. coordOf t  == from2d c) lvl
+findWall c lvl = preview _head . flip findStaticElement lvl $ \t ->
+  render (toTile t) == '#' && lvl ^. coordOf t  == from2d c
 
 drawGame :: GameState -> Picture
-drawGame s@(GameState (x,y) _ _ _ _) =
-  setCursor (x,y) . pic_for_image $ (drawLevel (s ^. level) <-> drawMessages (s ^. messages))
-                                <|> drawTaskManager (s ^. taskManager)
+drawGame s = setCursor (s ^. cursor) . pic_for_image $
+             (drawLevel (s ^. level) <-> drawMessages (s ^. messages))
+             <|> drawTaskManager (s ^. taskManager)
 
 drawLevel :: Level -> Image
 drawLevel = vert_cat . map (string def_attr) . lines . show
@@ -113,8 +118,8 @@ drawTaskManager tm = vert_cat . map (string def_attr) $
                      ]
 
 freshId :: GameState -> (Identifier a, GameState)
-freshId state = let (ident,cnt) = C.freshId (state ^. counter)
-                in (ident,state & counter .~ cnt)
+freshId state = (ident,state & counter .~ cnt)
+  where (ident,cnt) = C.freshId (state ^. counter)
 
 setCursor :: (Int, Int) -> Picture -> Picture
 setCursor (x,y) pic = pic { pic_cursor = Cursor (fromIntegral x) (fromIntegral y)}
